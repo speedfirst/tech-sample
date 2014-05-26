4e20986a7b408ba93f39c46c5b1fca96af957b31
Jiankuan Xing <jiankuan@yahoo-inc.com>
2014-05-26 10:58:14 +0800

    add localhost name test

---
+++ java-sample/src/test/java/org/speedfirst/test/LocalHostNameTest.java
@@ -0,0 +1,10 @@
+package org.speedfirst.test;
+
+import java.net.UnknownHostException;
+
+public class LocalHostNameTest {
+    public static void main(String[] args) throws UnknownHostException {
+        String localhostname = java.net.InetAddress.getLocalHost().getHostName();
+        System.out.println(localhostname);
+    }
+}
